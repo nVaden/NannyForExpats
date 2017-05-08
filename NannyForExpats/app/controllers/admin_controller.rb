35592e691658957c51a0a59a4d2aeff2e6f07041
Nicholas <vaden.nicholas@gmail.com>
2017-05-08 15:40:34 -0400

    family, nanny, admin view and listing

--- NannyForExpats/app/controllers/admin_controller.rb
+++ NannyForExpats/app/controllers/admin_controller.rb
@@ -1,2 +1,1 @@
 class AdminController < ApplicationController
-end
--- NannyForExpats/app/models/admin.rb
+++ NannyForExpats/app/models/admin.rb
@@ -1,9 +1,15 @@
 class Admin < ApplicationRecord
-  
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   mount_uploader :avatar, AvatarUploader
+ 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+ 
+  # User Avatar Validation
+  validates_integrity_of  :avatar
+  validates_processing_of :avatar
+ 
+  private
+    def avatar_size_validation
+      errors[:avatar] << "should be less than 500KB" if avatar.size > 0.5.megabytes
+    end
 end
